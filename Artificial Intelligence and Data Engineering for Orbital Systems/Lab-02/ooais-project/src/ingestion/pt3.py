19c3119c371e37eb98dd213c41eaab1da40e5c2b
SzeMrok <sljcichowski@gmail.com>
2026-04-01 03:25:13 +0200

    Add Lab-02

---
+++ Artificial Intelligence and Data Engineering for Orbital Systems/Lab-02/ooais-project/src/ingestion/pt3.py
@@ -0,0 +1,20 @@
+'''
+pt3-1
+Create a new dataset containing only selected columns (timestamp, object id, temperature).
+'''
+
+def pt3_1():
+    with open("data/raw/observations.csv", "r") as file:
+        lines = file.readlines()
+    
+    
+        
+    groups = []
+    
+    
+    for i in len(lines[0].strip().split(',')):
+        groups
+        
+    
+    
+pt3_1()
