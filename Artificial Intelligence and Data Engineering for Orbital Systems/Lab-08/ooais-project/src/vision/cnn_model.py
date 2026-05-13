9f1423f3d17fff181a2b718cac674909df7939d4
SzeMrok <sljcichowski@gmail.com>
2026-05-13 05:30:04 +0200

    Complete Lab-08

--- Artificial Intelligence and Data Engineering for Orbital Systems/Lab-08/ooais-project/src/vision/cnn_model.py
+++ Artificial Intelligence and Data Engineering for Orbital Systems/Lab-08/ooais-project/src/vision/cnn_model.py
@@ -6,30 +6,25 @@ class SimpleCNN(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(
-                in_channels=3,
-                out_channels=16,
-                kernel_size=3,
-                padding=1
-            ),
+            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2),
-            nn.Conv2d(
-                in_channels=16,
-                out_channels=32,
-                kernel_size=3,
-                padding=1
-            ),
+
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2)
         )
         self.classifier = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(32 * 16 * 16, 64),
+            nn.Linear(64 * 8 * 8, 128),
             nn.ReLU(),
-            nn.Linear(64, num_classes)
+            nn.Linear(128, num_classes)
         )
-    
+
     def forward(self, x):
         x = self.features(x)
         x = self.classifier(x)
